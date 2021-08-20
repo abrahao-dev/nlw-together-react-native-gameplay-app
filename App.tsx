bc6baa44e7d256defeb7e9693fbfb5fd0c9f92c9
mamalvares <stormtroper.rebels@gmail.com>
2021-08-20 10:04:25 -0300

    bugs

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import AppLoading from "expo-app-loading";
 import { useFonts } from 'expo-font';
 
 import { Routes } from './src/routes';
+import { Background } from './src/components/Background';
 
 export default function App(){
   const [fontsLoaded] = useFonts({
